2c6220818de47d349afb560f18c2b0d7aaa5c1f2
Alberto Mario Ceballos Arroyo <rzks95@gmail.com>
2017-10-07 21:45:39 -0500

    asdsad

--- r-group/config/settings/local.py
+++ r-group/config/settings/local.py
@@ -2,3 +2,14 @@ from .base import *
 
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='86mv^0+am3*@p=e%d1rp3q(@1nnm*8s@aee$vr*izd+&kxvp$&')
 DEBUG = env.bool('DJANGO_DEBUG', True)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'r-group',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
